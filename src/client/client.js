c17d6f089628b98bb5f107380c230a4243e7fe58
Clint Milner <clinton.milner@gmail.com>
2018-07-26 20:59:12 +0100

    SSR 07:30 implement StaticRouter and BrowserRouter so react-router-dom can use a master Routes file

--- src/client/client.js
+++ src/client/client.js
@@ -4,6 +4,11 @@ console.log('Client Side Code = client.js!');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>,
+    document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const port = 3000;
 
 app.use(express.static('public')); // public directory is available to browser
 
-app.get('/', (req, res) => {
-    res.send( renderer() );
+app.get('*', (req, res) => {
+    res.send( renderer(req) );
 });
 
 app.listen(port, () => {
